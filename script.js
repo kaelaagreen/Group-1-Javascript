316da08fab0c5a6618f9a941270b632184a30c34
Areddy28 <reddyanurag28@gmail.com>
2021-07-11 14:19:58 -0400

    Added a timer

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 alert("Group 1 RULES");
+
+var myTimer; 
+function clock() {
+    myTimer = setInterval(myClock, 1000);
+    var i = 30;
+
+    function myClock() {
+        document.getElementById("demo").innerHTML = i--;
+        if (i == 0) {
+          clearInterval(myTimer);
+          alert("Reached zero");
+        }
+      }
+    }
