cb8ce4c1fadecaef993b2e59ddbca73bec50e419
Areddy28 <reddyanurag28@gmail.com>
2021-07-12 01:18:31 -0400

    Timer

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 alert("Group 1 RULES");
 
+// What is the setInterval() in relation with the clearInterval() method and how it works together?
+//The setInterval() method calls a function or evaluates an expression at specified intervals (in milliseconds).  
+//The setInterval() method will continue calling the function until clearInterval() is called, or the window is closed.
+
 
 let timer = document.querySelector('h3')
 let startingTime = 30
 let startTimer;
 let startStopTimer = () => {
-  if(startTimer){
+
+if(startTimer){
     clearInterval(startTimer);
     timer.innerHTML = '00:30'
     startingTime = 30
